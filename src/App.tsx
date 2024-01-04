db1bfc64c40898db074eae62e6a144723cdc3f92
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2024-01-04 16:13:45 -0500

    install MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Drawer from "./components/Drawer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<h1>Home</h1>} />
-      <Route path="about" element={<h1>About</h1>} />
-    </Routes>
+    <Drawer>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="slow-api" element={<h1>About</h1>} />
+      </Routes>
+    </Drawer>
   );
 }
 
